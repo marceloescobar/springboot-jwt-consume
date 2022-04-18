556e6ea9d34204c936c44b27b2fac38d35c28377
MEscobar <escobar.marcelo@gmail.com>
2022-04-18 12:25:46 -0300

    commit inicial

---
+++ src/main/java/com/mescobar/jwtconsume/model/ResponseToken.java
@@ -0,0 +1,16 @@
+package com.mescobar.jwtconsume.model;
+
+public class ResponseToken {
+
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	
+}
---
+++ src/test/java/com/mescobar/jwtconsume/SpringBootJwtConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mescobar.jwtconsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtConsumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
